d122e5853a9d71a8fc498c03a2d70b17ff421f4f
Guest User <student@epicodus.com>
2015-09-09 16:26:44 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// var palindrome = function(string) {
+//   return ((string.split("")).join()) === ((string.split("").reverse()).join());
+//
+// };
+
+var palindrome = function(string) {
+  var a = string.split("");
+  var b = string.split("").reverse();
+    for (var i = 0; i >= a.length; i++);
+      (a[i] == b[i]) {
+      } else {
+        return false;
+      }
+  }
+};
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe('palindrome', function(){
+  it('it determines if an input is a palindrome', function () {
+    expect(palindrome('noon')).to.equal(true)
+  });
+
+});
